310e4ff9cd973ef2bd7920e2f8cf3286bb0bcec3
savithaR24 <157411007+savithaR24@users.noreply.github.com>
2024-03-04 13:09:21 -0500

    Detail Screen Navigation
    Added Navigating to Detail screen and back plus other UI fixes

--- app/src/main/java/com/kodeco/android/countryinfo/ui/components/CountryDetailsScreen.kt
+++ app/src/main/java/com/kodeco/android/countryinfo/ui/components/CountryDetailsScreen.kt
@@ -1,10 +1,25 @@
 package com.kodeco.android.countryinfo.ui.components
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -15,20 +30,46 @@ import coil.request.ImageRequest
 import com.kodeco.android.countryinfo.models.Country
 
 @Composable
-fun CountryDetailsScreen(country: Country) {
-    LazyColumn {
-        item { Text(text = "Capital: ${country.nonNullCapital}") }
-        item { Text(text = "Population: ${country.population}") }
-        item { Text(text = "Area: ${country.area}") }
-        item {
-            AsyncImage(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(country.flagUrl)
-                    .crossfade(true)
-                    .build(),
-                contentDescription = "Flag",
-                contentScale = ContentScale.Fit,
+fun CountryDetailsScreen(country: Country, onBackPress: () -> Unit) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(country.commonName)
+                },
+                navigationIcon = {
+                    IconButton(onClick = onBackPress) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                }
             )
+        }
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            item { Text(text = "Capital: ${country.nonNullCapital}") }
+            item { Text(text = "Population: ${country.population}") }
+            item { Text(text = "Area: ${country.area}") }
+            item {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(country.flagUrl)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = "Flag",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier
+                        .size(200.dp)
+                )
+            }
         }
     }
 }
@@ -36,5 +77,8 @@ fun CountryDetailsScreen(country: Country) {
 @Preview
 @Composable
 fun CountryDetailsScreenPreview() {
-    CountryDetailsScreen(country = sampleCountry)
+    CountryDetailsScreen(
+        country = sampleCountry,
+        onBackPress = {}
+    )
 }
--- app/src/main/java/com/kodeco/android/countryinfo/ui/components/CountryInfoList.kt
+++ app/src/main/java/com/kodeco/android/countryinfo/ui/components/CountryInfoList.kt
@@ -1,26 +1,47 @@
 package com.kodeco.android.countryinfo.ui.components
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.kodeco.android.countryinfo.models.Country
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 
 @Composable
 fun CountryInfoList(countries: List<Country>) {
     var selectedCountry: Country? by remember { mutableStateOf(null) }
 
-    if (selectedCountry != null) {
-        CountryDetailsScreen(selectedCountry!!)
-    } else {
-        LazyColumn {
-            items(countries) {
-                CountryInfoRow(it) {
-                    selectedCountry = it
+    selectedCountry?.let {
+        CountryDetailsScreen(it, onBackPress = {
+            selectedCountry = null
+        })
+    } ?: run {
+        Scaffold (
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text(text = "Countries of the World")
+                    }
+                )
+            }
+        ) {
+            LazyColumn(
+                modifier = Modifier
+                    .padding(it)
+            ) {
+                items(countries) {
+                    CountryInfoRow(it) {
+                        selectedCountry = it
+                    }
                 }
             }
         }

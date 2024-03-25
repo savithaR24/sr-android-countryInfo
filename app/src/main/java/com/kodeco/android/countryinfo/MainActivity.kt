2a9daf42c88015af55cb3590e6abce6983da98b0
savithaR24 <157411007+savithaR24@users.noreply.github.com>
2024-03-25 16:47:24 -0400

    Fix crash on screen orientation change
    When screen is changed from Portrait to landscape or vice-versa and then country row is tapped to display the details, the app would crash since repository is wiped out on orientation change but view model stays. This commit fixes the problem.

--- app/src/main/java/com/kodeco/android/countryinfo/MainActivity.kt
+++ app/src/main/java/com/kodeco/android/countryinfo/MainActivity.kt
@@ -3,35 +3,17 @@ package com.kodeco.android.countryinfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.kodeco.android.countryinfo.api.CountryAPIService
 import com.kodeco.android.countryinfo.nav.CountryInfoNavHost
-import com.kodeco.android.countryinfo.repositories.CountryRepositoryImpl
 import com.kodeco.android.countryinfo.ui.theme.MyApplicationTheme
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 
-private const val BASE_URL = "https://restcountries.com/v3.1/"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val moshi = Moshi.Builder()
-            .add(KotlinJsonAdapterFactory())
-            .build()
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .build()
-
-        val apiService: CountryAPIService = retrofit.create(CountryAPIService::class.java)
-
         setContent {
             MyApplicationTheme {
-                CountryInfoNavHost(repository = CountryRepositoryImpl(apiService))
+                CountryInfoNavHost(repository = (application as CountryInfoApp).repository)
             }
         }
     }
